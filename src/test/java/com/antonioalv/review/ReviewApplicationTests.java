993e00f84b4eb419a9b201b147c63527307deea2
antonio36alv <ant.36alv@gmail.com>
2020-11-23 21:44:01 -0500

    cleaned up code and formatted correctly

--- src/test/java/com/antonioalv/review/ReviewApplicationTests.java
+++ src/test/java/com/antonioalv/review/ReviewApplicationTests.java
@@ -1,23 +1,16 @@
 package com.antonioalv.review;
 
 import com.antonioalv.review.model.ChatMessage;
-import org.junit.Rule;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.containers.Network;
@@ -38,69 +31,50 @@ class ReviewApplicationTests {
 	public int port;
 
     @Container
-	public BrowserWebDriverContainer chrome =
+	public static BrowserWebDriverContainer firefox =
 			(BrowserWebDriverContainer) new BrowserWebDriverContainer()
 					.withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL,
-							new File("./target/"))
-					.withCapabilities(new FirefoxOptions()
-							.addArguments("--headless",
-									"--disable-gpu",
-									"--window-size=1920,1200",
-									"--ignore-certificate-errors"))
-				.withNetwork(Network.SHARED);
+										new File("./target/"))
+					.withCapabilities(new FirefoxOptions().addArguments("--headless",
+																		"--disable-gpu",
+																		"--window-size=1920,1200",
+																		"--ignore-certificate-errors"))
+					.withNetwork(Network.SHARED);
+
+	public static RemoteWebDriver driver;
 
 	public String baseURL;
+	public static String ip = "";
 
-//	@BeforeAll
-//	public static void beforeAll() {
-//		driver = chrome.getWebDriver();
-		/*
-		previously used
-		// WebDriverManager.chromedriver().setup();
-		// driver = new ChromeDriver();
-		// for windows
-		// String firefoxDriverPath = "C:\\Program Files (x86)\\firefoxdriver.exe";
-		// for linux
-		// String chromeDriverPath = "/usr/bin/chromedriver";
-		// System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		// driver = new ChromeDriver(options);
-		*/
-//		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
-//        chrome = new BrowserWebDriverContainer<>().withCapabilities(options);
-//		chrome = new BrowserWebDriverContainer<>().withCapabilities(new ChromeOptions().addArguments());
-//		driver = chrome.getWebDriver();
-//		String firefoxDriverPath = "/usr/local/bin/geckodriver";
-//		System.setProperty("webdriver.gecko.driver", firefoxDriverPath);
-//	}
+	@BeforeAll
+	public static void beforeAll() {
+		// init our remote web driver
+		driver = firefox.getWebDriver();
+		// in order to find host ip address where our app server is running
+		// https://stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
+		try(final DatagramSocket socket = new DatagramSocket()){
+			socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+			ip = socket.getLocalAddress().getHostAddress();
+		} catch (UnknownHostException | SocketException e) {
+			e.printStackTrace();
+		}
+	}
 
-//	@AfterAll
-//	public static void afterAll() {
-//		driver.quit();
-//		driver = null;
-//	}
+	@AfterAll
+	public static void afterAll() {
+		driver.quit();
+		driver = null;
+	}
 
 	@BeforeEach
 	public void beforeEach() {
-//		baseURL = baseURL = "http://localhost:" + port;
-		String hostIpAddress = chrome.getHost();
-		System.out.println("here");
-		System.out.println("here");
-		System.out.println("here");
-		System.out.println("here");
-		System.out.println("here");
-		System.out.println("here");
-		System.out.println(hostIpAddress);
-//		baseURL = "http://" + hostIpAddress + ":" + port;
-//		baseURL = "http://" + hostIpAddress + ":" + port;
+		baseURL = String.format("http://%s:%d", ip, port);
 		org.testcontainers.Testcontainers.exposeHostPorts(port);
-		chrome.addExposedPort(port);
-		System.out.println(chrome.getExposedPorts());
+		firefox.addExposedPort(port);
 	}
 
 	@Test
 	public void userSignLoginChatTest() {
-		RemoteWebDriver driver = chrome.getWebDriver();
 
 		String firstName = "Buckley";
 		String lastName = "Payne";
@@ -110,21 +84,6 @@ class ReviewApplicationTests {
 
 		String messageText = "Smell the meat, not the heat";
 
-		System.out.println("i want pizza");
-		System.out.println(chrome.getHost());
-
-//		String hostIpAddress = "192.168.1.155";
-		// https://stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
-		String ip = "";
-		try(final DatagramSocket socket = new DatagramSocket()){
-			socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
-			ip = socket.getLocalAddress().getHostAddress();
-			System.out.println(String.format("my ip: %s", ip));
-		} catch (UnknownHostException | SocketException e) {
-			e.printStackTrace();
-		}
-		baseURL = "http://" + ip + ":" + port;
-		System.out.println(baseURL);
 		driver.get(baseURL + "/signup");
 
 		SignupPage signup = new SignupPage(driver);
